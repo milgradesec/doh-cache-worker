6e9e335a90f7f62c93533553a9f435f75fb77657
milgradesec <38557680+milgradesec@users.noreply.github.com>
2021-10-28 07:56:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,12 +9,6 @@ addEventListener("fetch", event => {
     }
 })
 
-function base64Encode(byteArray) {
-    return btoa(Array.from(new Uint8Array(byteArray)).map(val => {
-        return String.fromCharCode(val);
-    }).join('')).replace(/\+/g, '-').replace(/\//g, '_').replace(/\=/g, '');
-}
-
 async function handleRequest(event) {
     const request = event.request
     const cache = caches.default
@@ -23,24 +17,35 @@ async function handleRequest(event) {
     const body = await request.arrayBuffer()
     const encodedBody = base64Encode(body);
 
-    // Create a GET request with encoded body as dns param.
+    // Create a request URL with encoded body as query parameter.
     const url = new URL("https://dns.paesa.es/dns-query");
     url.searchParams.append("dns", encodedBody)
-
-    const newRequestInit = {
-        method: "GET",
-        body: null,
-    };
-
-    const attrs = new Request(request, newRequestInit);
-    const newRequest = new Request(url.href, attrs);
 
     // Check if response is cached at edge.
     const cacheKey = url.toString()
     let response = await cache.match(cacheKey)
     if (!response) {
+        console.log("Response served from origin.")
+        
+        // Create a GET request from the original POST request. 
+        const newRequestInit = {
+            method: "GET",
+            body: null,
+        };
+        const attrs = new Request(request, newRequestInit);
+        const newRequest = new Request(url.href, attrs);
+
+        // Fetch response from origin.
         response = await fetch(newRequest)
         event.waitUntil(cache.put(cacheKey, response.clone()))
+    } else {
+        console.log("Response served from cache.")
     }
     return response
+}
+
+function base64Encode(byteArray) {
+    return btoa(Array.from(new Uint8Array(byteArray)).map(val => {
+        return String.fromCharCode(val);
+    }).join('')).replace(/\+/g, '-').replace(/\//g, '_').replace(/\=/g, '');
 }
