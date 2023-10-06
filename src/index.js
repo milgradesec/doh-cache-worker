36491eaca29074c797f85ef1050a3f401f301ea2
milgradesec <38557680+milgradesec@users.noreply.github.com>
2023-10-06 19:08:48 +0200

    Add prettier

--- src/index.js
+++ src/index.js
@@ -1,38 +1,45 @@
 export default {
-    async fetch(request, env) {
-        // Only handle POST requests.
-        if (request.method === "POST") {
-            return handleRequest(request, env)
-        }
-        return fetch(request)
-    },
-};
+  async fetch(request, env) {
+    // Only handle POST requests.
+    if (request.method === 'POST') {
+      return handleRequest(request, env)
+    }
+    return fetch(request)
+  },
+}
 
 async function handleRequest(request, env) {
-    // Base64 encode request body.
-    const body = await request.arrayBuffer()
-    const encodedBody = base64Encode(body);
+  // Base64 encode request body.
+  const body = await request.arrayBuffer()
+  const encodedBody = base64Encode(body)
 
-    // Create a request URL with encoded body as query parameter.
-    const url = new URL(`https://${env.DOH_ENDPOINT}`);
-    url.searchParams.append("dns", encodedBody)
+  // Create a request URL with encoded body as query parameter.
+  const url = new URL(`https://${env.DOH_ENDPOINT}`)
+  url.searchParams.append('dns', encodedBody)
 
-    // Create a GET request from the original POST request. 
-    const getRequest = new Request(url.href, {
-        method: "GET",
-        body: null,
-    });
+  // Create a GET request from the original POST request.
+  const getRequest = new Request(url.href, {
+    method: 'GET',
+    body: null,
+  })
 
-    // Fetch response from origin server.
-    return await fetch(getRequest, {
-        cf: {
-            cacheEverything: true,
-        },
-    })
+  // Fetch response from origin server.
+  return await fetch(getRequest, {
+    cf: {
+      cacheEverything: true,
+    },
+  })
 }
 
 function base64Encode(byteArray) {
-    return btoa(Array.from(new Uint8Array(byteArray)).map(val => {
-        return String.fromCharCode(val);
-    }).join('')).replace(/\+/g, '-').replace(/\//g, '_').replace(/\=/g, '');
+  return btoa(
+    Array.from(new Uint8Array(byteArray))
+      .map((val) => {
+        return String.fromCharCode(val)
+      })
+      .join(''),
+  )
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/\=/g, '')
 }
